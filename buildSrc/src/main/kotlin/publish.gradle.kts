3900b9e1c774ead8f295011b275f5a46441b03e5
hsicen <codinghuang@163.com>
2022-05-13 22:38:20 +0800

    update.

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -1,70 +1,70 @@
 plugins {
-    id("maven-publish")
-    id("signing")
+  id("maven-publish")
+  id("signing")
 }
 
 afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            create<MavenPublication>("release") {
-                // Applies the component for the release buildTypes.
-                if (plugins.hasPlugin("com.android.library")) {
-                    from(components["release"]) // android-aar include code file and res
-                } else {
-                    from(components["java"]) //java-jar include code file
-                }
-
-                // You can then customize attributes of the publication as shown below.
-                groupId = "io.github.hsicen"
-                artifactId = "toast"
-                version = Sdk.versionName
-
-                pom {
-                    name.set("toast")
-                    description.set("A concise description of my library")
-                    url.set("http://www.example.com/library")
-                    properties.set(
-                        mapOf(
-                            "myProp" to "value",
-                            "prop.with.dots" to "anotherValue"
-                        )
-                    )
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("hsicen")
-                            name.set("hsicen")
-                            email.set("hsiceni@gmail.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://example.com/my-library.git")
-                        developerConnection.set("scm:git:ssh://example.com/my-library.git")
-                        url.set("http://example.com/my-library/")
-                    }
-                }
-            }
+  publishing {
+    publications {
+      // Creates a Maven publication called "release".
+      create<MavenPublication>("release") {
+        // Applies the component for the release buildTypes.
+        if (plugins.hasPlugin("com.android.library")) {
+          from(components["release"]) // android-aar include code file and res
+        } else {
+          from(components["java"]) //java-jar include code file
         }
 
-        repositories {
-            maven {
-                name = "toast"
-                url = uri(properties["mavenUrl"].toString())
-                credentials {
-                    username = properties["mavenName"].toString()
-                    password = properties["mavenPwd"].toString()
-                }
+        // You can then customize attributes of the publication as shown below.
+        groupId = "io.github.hsicen"
+        artifactId = "toast"
+        version = Sdk.versionName
+
+        pom {
+          name.set("toast")
+          description.set("A concise description of my library")
+          url.set("http://www.example.com/library")
+          properties.set(
+            mapOf(
+              "myProp" to "value",
+              "prop.with.dots" to "anotherValue"
+            )
+          )
+          licenses {
+            license {
+              name.set("The Apache License, Version 2.0")
+              url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+          }
+          developers {
+            developer {
+              id.set("hsicen")
+              name.set("hsicen")
+              email.set("hsiceni@gmail.com")
+            }
+          }
+          scm {
+            connection.set("scm:git:git://example.com/my-library.git")
+            developerConnection.set("scm:git:ssh://example.com/my-library.git")
+            url.set("http://example.com/my-library/")
+          }
         }
+      }
     }
 
-    signing {
-        sign(publishing.publications.getByName("release"))
+    repositories {
+      maven {
+        name = "toast"
+        url = uri(properties["mavenUrl"].toString())
+        credentials {
+          username = properties["mavenName"].toString()
+          password = properties["mavenPwd"].toString()
+        }
+      }
     }
+  }
+
+  signing {
+    sign(publishing.publications.getByName("release"))
+  }
 }
--- toast/build.gradle.kts
+++ toast/build.gradle.kts
@@ -1,42 +1,42 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-    publish
+  id("com.android.library")
+  kotlin("android")
+  publish
 }
 
 android {
-    compileSdk = Sdk.compileSdk
+  compileSdk = Sdk.compileSdk
 
-    defaultConfig {
-        minSdk = Sdk.minSdk
-        targetSdk = Sdk.targetSdk
-        versionCode = Sdk.versionCode
-        versionName = Sdk.versionName
+  defaultConfig {
+    minSdk = Sdk.minSdk
+    targetSdk = Sdk.targetSdk
+    versionCode = Sdk.versionCode
+    versionName = Sdk.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
-    implementation("androidx.core:core-ktx:1.5.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("com.github.hsicen:Extensions:1.0.6")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
+  implementation("androidx.core:core-ktx:1.5.0")
+  implementation("androidx.appcompat:appcompat:1.3.0")
+  implementation("com.google.android.material:material:1.3.0")
+  implementation("com.github.hsicen:Extensions:1.0.6")
 }
